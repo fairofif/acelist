810d72d4dc40896eebff797178a045c7f6722739
cinnamonboyz <18220012@std.stei.itb.ac.id>
2022-11-20 20:57:00 +0700

    Book History and Book History Details Implemented

--- src/Acelist/boundary/BookHistoryUI.Designer.cs
+++ src/Acelist/boundary/BookHistoryUI.Designer.cs
@@ -28,33 +28,202 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ColumnID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnRoom = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnCustomer = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnCheckin = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnCheckout = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnDetails = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // panel1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(337, 117);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 25);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "test";
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(47)))), ((int)(((byte)(51)))));
+            this.panel1.Controls.Add(this.buttonSearch);
+            this.panel1.Controls.Add(this.textBoxSearch);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(891, 85);
+            this.panel1.TabIndex = 0;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(101)))), ((int)(((byte)(242)))));
+            this.buttonSearch.FlatAppearance.BorderSize = 0;
+            this.buttonSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSearch.Font = new System.Drawing.Font("Segoe UI Semibold", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonSearch.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonSearch.Location = new System.Drawing.Point(204, 27);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(86, 29);
+            this.buttonSearch.TabIndex = 1;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = false;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(39)))), ((int)(((byte)(42)))));
+            this.textBoxSearch.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxSearch.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxSearch.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBoxSearch.Location = new System.Drawing.Point(31, 27);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(158, 29);
+            this.textBoxSearch.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(39)))), ((int)(((byte)(42)))));
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 85);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(891, 449);
+            this.panel2.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(47)))), ((int)(((byte)(51)))));
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(101)))), ((int)(((byte)(242)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnID,
+            this.ColumnRoom,
+            this.ColumnCustomer,
+            this.ColumnCheckin,
+            this.ColumnCheckout,
+            this.ColumnDetails});
+            this.dataGridView1.EnableHeadersVisualStyles = false;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.dataGridView1.Location = new System.Drawing.Point(21, 24);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 62;
+            dataGridViewCellStyle8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(39)))), ((int)(((byte)(42)))));
+            dataGridViewCellStyle8.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle8.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(231)))), ((int)(((byte)(92)))));
+            dataGridViewCellStyle8.SelectionForeColor = System.Drawing.Color.Black;
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle8;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(849, 403);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // ColumnID
+            // 
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ColumnID.DefaultCellStyle = dataGridViewCellStyle2;
+            this.ColumnID.HeaderText = "Booking ID";
+            this.ColumnID.MinimumWidth = 8;
+            this.ColumnID.Name = "ColumnID";
+            // 
+            // ColumnRoom
+            // 
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ColumnRoom.DefaultCellStyle = dataGridViewCellStyle3;
+            this.ColumnRoom.HeaderText = "Room";
+            this.ColumnRoom.MinimumWidth = 8;
+            this.ColumnRoom.Name = "ColumnRoom";
+            // 
+            // ColumnCustomer
+            // 
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ColumnCustomer.DefaultCellStyle = dataGridViewCellStyle4;
+            this.ColumnCustomer.HeaderText = "Customer ID";
+            this.ColumnCustomer.MinimumWidth = 8;
+            this.ColumnCustomer.Name = "ColumnCustomer";
+            // 
+            // ColumnCheckin
+            // 
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ColumnCheckin.DefaultCellStyle = dataGridViewCellStyle5;
+            this.ColumnCheckin.HeaderText = "Checkin Date";
+            this.ColumnCheckin.MinimumWidth = 8;
+            this.ColumnCheckin.Name = "ColumnCheckin";
+            // 
+            // ColumnCheckout
+            // 
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ColumnCheckout.DefaultCellStyle = dataGridViewCellStyle6;
+            this.ColumnCheckout.HeaderText = "Checkout Date";
+            this.ColumnCheckout.MinimumWidth = 8;
+            this.ColumnCheckout.Name = "ColumnCheckout";
+            // 
+            // ColumnDetails
+            // 
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle7.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(231)))), ((int)(((byte)(92)))));
+            dataGridViewCellStyle7.Padding = new System.Windows.Forms.Padding(5);
+            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(69)))), ((int)(((byte)(158)))));
+            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(87)))), ((int)(((byte)(242)))), ((int)(((byte)(135)))));
+            this.ColumnDetails.DefaultCellStyle = dataGridViewCellStyle7;
+            this.ColumnDetails.HeaderText = "";
+            this.ColumnDetails.MinimumWidth = 8;
+            this.ColumnDetails.Name = "ColumnDetails";
+            this.ColumnDetails.Text = "Details";
+            this.ColumnDetails.UseColumnTextForButtonValue = true;
             // 
             // BookHistoryUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(891, 534);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Name = "BookHistoryUI";
             this.Text = "Book History";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private Label label1;
+        private Panel panel1;
+        private Panel panel2;
+        private Button buttonSearch;
+        private TextBox textBoxSearch;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn ColumnID;
+        private DataGridViewTextBoxColumn ColumnRoom;
+        private DataGridViewTextBoxColumn ColumnCustomer;
+        private DataGridViewTextBoxColumn ColumnCheckin;
+        private DataGridViewTextBoxColumn ColumnCheckout;
+        private DataGridViewButtonColumn ColumnDetails;
     }
 }
--- src/Acelist/boundary/BookHistoryUI.cs
+++ src/Acelist/boundary/BookHistoryUI.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Acelist.boundary.popupform;
+using Acelist.entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +15,89 @@ namespace Acelist.boundary
 {
     public partial class BookHistoryUI : Form
     {
-        public BookHistoryUI()
+        private Booking booking = new Booking();
+        private Panel panelmainmenu;
+        public BookHistoryUI(Panel panelmainmenu)
         {
             InitializeComponent();
+            printHistoryToTable();
+            this.panelmainmenu = panelmainmenu;
+        }
+
+        private void printHistoryToTable()
+        {
+            int bookID;
+            int roomID;
+            string custID;
+            string checkinDate;
+            string checkoutDate;
+            this.dataGridView1.Rows.Clear();
+            for (int i = 0; i < booking.getArrBookingID().Count; i++)
+            {
+                if (booking.getArrHasCheckedOut()[i] == true)
+                {
+                    bookID = booking.getArrBookingID()[i];
+                    roomID = booking.getArrRoomID()[i];
+                    custID = booking.getArrCustomerID()[i];
+                    checkinDate = booking.getArrCheckinTime()[i].ToShortDateString();
+                    checkoutDate = booking.getArrCheckoutTime()[i].ToShortDateString();
+                    this.dataGridView1.Rows.Add(bookID,roomID,custID, checkinDate,checkoutDate);
+
+                }
+            }
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            if (this.textBoxSearch.Text == "")
+            {
+                printHistoryToTable();
+            }
+            else
+            {
+                int bookID = Int32.Parse(textBoxSearch.Text);
+                this.dataGridView1.Rows.Clear();
+                int i = 0;
+                bool found = false;
+                while (found == false && i < booking.getArrBookingID().Count)
+                {
+                    if (bookID == booking.getArrBookingID()[i] && booking.getArrHasCheckedOut()[i] == true)
+                    {
+                        found = true;
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                }
+                if (found)
+                {
+                    dataGridView1.Rows.Add(bookID, booking.getArrRoomID()[i], booking.getArrCustomerID()[i], booking.getArrCheckinTime()[i].ToShortDateString(), booking.getArrCheckoutTime()[i].ToShortDateString());
+                }
+                
+            }
+        }
+
+        private void callHistoryDetails(int iddetail)
+        {
+            HistoryDetails history = new HistoryDetails(panelmainmenu, this, iddetail);
+            history.TopLevel = false;
+            history.FormBorderStyle = FormBorderStyle.None;
+            history.Dock = DockStyle.Fill;
+            this.Hide();
+            this.panelmainmenu.Controls.Add(history);
+            this.panelmainmenu.Tag = history;
+            history.BringToFront();
+            history.Show();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 5 && e.RowIndex >= 0)
+            {
+                int iddetail = Int32.Parse(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+                callHistoryDetails(iddetail);
+            }
         }
     }
 }
